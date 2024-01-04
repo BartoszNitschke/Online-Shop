f7447e592ecf694e68c95944a4be6bf04def6318
BartoszNitschke <s25771@pjwstk.edu.pl>
2024-01-04 01:07:31 +0100

    backend setup for products

---
+++ backend/controllers/productController.js
@@ -0,0 +1,88 @@
+const Product = require("../models/Product");
+const mongoose = require("mongoose");
+
+const getProducts = async (req, res) => {
+  const products = await Product.find({});
+
+  res.status(200).json(products);
+};
+
+const getProduct = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No product with this id" });
+  }
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    return res.status(404).json({ error: "No product with this id" });
+  }
+
+  res.status(200).json(product);
+};
+
+const addProduct = async (req, res) => {
+  const { name, url, priceNoDelivery, priceDelivery, description, quantity } =
+    req.body;
+
+  try {
+    const product = await Product.create({
+      name,
+      url,
+      priceNoDelivery,
+      priceDelivery,
+      description,
+      quantity,
+    });
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No product with this id" });
+  }
+
+  const product = await Product.findOneAndDelete({ _id: id });
+
+  if (!product) {
+    return res.status(404).json({ error: "No product with this id" });
+  }
+
+  res.status(200).json(product);
+};
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No product with this id" });
+  }
+
+  const product = await Product.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!product) {
+    return res.status(404).json({ error: "No product with this id" });
+  }
+
+  res.status(200).json(product);
+};
+
+module.exports = {
+  addProduct,
+  getProducts,
+  getProduct,
+  deleteProduct,
+  updateProduct,
+};
---
+++ backend/models/Product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    priceNoDelivery: {
+      type: Number,
+      required: true,
+    },
+    priceDelivery: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
